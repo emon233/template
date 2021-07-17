fe112c4b51deb465886a6fff082501cf841a9650
emon233 <sazid.hossain.1433@gmail.com>
2021-07-17 13:32:13 +0600

    Initial Commit

---
+++ app/Helpers/constants.php
@@ -0,0 +1,7 @@
+<?php 
+
+const SITE_NAME = "AdminLTE";
+const SITE_NAME_SHORT = "A LTE";
+
+const SITE_LOGO_PRIMARY = "assets/adminLte/dist/img/AdminLTELogo.png";
+const SITE_LOGO_SECONDARY = "assets/adminLte/dist/img/AdminLTELogo.png";
---
+++ resources/views/admin.blade.php
@@ -0,0 +1,2 @@
+@extends('adminLte.master')
+
