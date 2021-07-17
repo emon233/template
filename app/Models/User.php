88cd3adc92b27985d211168844b85e7c4c89aa9a
emon233 <sazid.hossain.1433@gmail.com>
2021-07-17 16:07:26 +0600

    Role Added

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 use App\Traits\Uuids;
 use OwenIt\Auditing\Contracts\Auditable;
@@ -13,7 +14,7 @@ use OwenIt\Auditing\Contracts\Auditable;
 class User extends Authenticatable implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
-    use HasFactory, Notifiable, Uuids;
+    use HasFactory, Notifiable, Uuids, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -21,8 +22,8 @@ class User extends Authenticatable implements Auditable
      * @var array
      */
     protected $fillable = [
-        'name',
-        'email',
+        'first_name', 'last_name',
+        'email', 'phone_no',
         'password',
     ];
 
@@ -43,5 +44,6 @@ class User extends Authenticatable implements Auditable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'phone_no_verified_at' => 'datetime',
     ];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name');
+            $table->uuid('role_id');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone_no')->nullable();
+            $table->timestamp('phone_no_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
