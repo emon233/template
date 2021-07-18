8ed964d9a16736a1b195e3f5289120dc4f094eb6
emon233 <sazid.hossain.1433@gmail.com>
2021-07-18 19:04:49 +0600

    SweetAlert Added with Signout

--- resources/views/adminLte/partials/scripts.blade.php
+++ resources/views/adminLte/partials/scripts.blade.php
@@ -2,6 +2,10 @@
 <script src="{{ asset('assets/adminLte/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
 <script src="{{ asset('assets/adminLte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- SweetAlert2 -->
+<script src="{{ asset('assets/adminLte/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+
 <!-- AdminLTE App -->
 <script src="{{ asset('assets/adminLte/dist/js/adminlte.min.js') }}"></script>
 
@@ -9,11 +13,39 @@
     $(document).ready(function() {
         $('#btn-signout').on('click', function(e) {
             e.preventDefault();
-            if (confirm('Are You Sure?')) {
-                $('#form-signout').submit();
-            } else {
-                alert("Operation Canceled");
-            }
+            Swal.fire({
+                title: 'Are you sure?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, Signout!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $('#form-signout').submit();
+                } else {
+                    cancelOperation("error", "Operation Canceled");
+                }
+            })
         })
     })
+
+    function cancelOperation(op = null, msg = null) {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        Toast.fire({
+            icon: op ? op : "success",
+            title: msg
+        })
+    }
+
 </script>
--- resources/views/adminLte/partials/styles.blade.php
+++ resources/views/adminLte/partials/styles.blade.php
@@ -2,5 +2,7 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 <!-- Font Awesome Icons -->
 <link rel="stylesheet" href="{{ asset('assets/adminLte/plugins/fontawesome-free/css/all.min.css') }}">
+<!-- SweetAlert2 -->
+<link rel="stylesheet" href="{{ asset('assets/adminLte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
 <!-- Theme style -->
 <link rel="stylesheet" href="{{ asset('assets/adminLte/dist/css/adminlte.min.css') }}">
