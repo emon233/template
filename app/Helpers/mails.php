7dc6d77c743256a96f3ffcb65b9845e8176ecbae
emon233 <sazid.hossain.1433@gmail.com>
2021-07-27 16:15:07 +0600

    Email Helper Class Added

---
+++ app/Helpers/mails.php
@@ -0,0 +1,9 @@
+<?php
+
+const EMAIL_ERROR_REPORT = "sazid.hossain.1433@gmail.com";
+
+const EMAIL_FROM = "sazid.hossain.1433@gmail.com";
+
+const SITE_URL = "http://personal.adminlte31.local/";
+const SITE_URL_SIGNIN = SITE_URL . "signin";
+const SITE_URL_LOG = SITE_URL . "logs";
