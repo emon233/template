240b1e1fab964ac4d60dd58791aec7587f6b5025
emon233 <sazid.hossain.1433@gmail.com>
2021-07-27 22:22:39 +0600

    Seeder Modified

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,24 +15,19 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $roles = Role::orderBy('priority', 'desc')->get();
+        $role = Role::where('all_access', true)->first();
 
-        foreach($roles as $role) {
-            $users = $role->priority == 100 ? 1:4;
-            for($i=0; $i<$users; $i++) {
-                User::create([
-                    'role_id' => $role->id,
-                    'first_name' => $role->title,
-                    'last_name' => 'User ' . $i,
-                    'email' => str_replace(' ', '-', strtolower($role->title)) . $i .'@adminlte.laravel',
-                    'email_verified_at' => now(),
-                    'phone_no' => null,
-                    'phone_no_verified_at' => null,
-                    'password' => bcrypt('password'),
-                    'created_at' => now(),
-                    'updated_at' => now()
-                ]);
-            }
-        }
+        User::create([
+            'role_id' => $role->id,
+            'first_name' => "System",
+            'last_name' => "Administrator",
+            'email' => 'sazid.hossain.1433@gmail.com',
+            'email_verified_at' => now(),
+            'phone_no' => '+8801311022920',
+            'phone_no_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
