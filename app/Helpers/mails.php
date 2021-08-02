d282222f89387b645560fb6ca1151bd361dc2d3d
emon233 <sazid.hossain.1433@gmail.com>
2021-08-02 14:13:24 +0600

    Mail Constants Modified

--- app/Helpers/mails.php
+++ app/Helpers/mails.php
@@ -11,51 +11,24 @@ const EMAIL_ERROR_REPORT = "sazid.hossain.1433@gmail.com";
 const EMAIL_FROM = "system@personal.adminlte31.local";
 
 const SITE_URL = "http://personal.adminlte31.local/";
+
 const SITE_URL_SIGNIN = SITE_URL . "signin";
 const SITE_URL_LOG = SITE_URL . "logs";
 
+const SITE_LOGO_MAIL = "assets/adminLte/dist/img/AdminLTELogo.png";
 
-/**
- * Save Mail Error To Log
- *
- * @param [type] $message
- * @return void
- */
-function mailError($message = null)
-{
-    if (!is_null($message)) {
-        Log::error($message);
-    }
-}
+/** Allowed Mail Constants */
 
-/**
- * Send Signup Notification Mail to Admin
- *
- * @param [type] $user
- * @return void
- */
-function sendSignupMail($user)
-{
-    try {
-        return Mail::to(EMAIL_SIGNUP_REPORT)->send(new UserSignup($user));
-    } catch (\Exception $ex) {
-        mailError($ex->getMessage());
-        return false;
-    }
-}
+const ALLOWED_MAILS = true;
 
-/**
- * Send Welcome Mail to User
- *
- * @param [type] $user
- * @return void
- */
-function sendWelcomeMail($user)
-{
-    try {
-        return Mail::to($user->email)->send(new Welcome);
-    } catch (\Exception $ex) {
-        mailError($ex->getMessage());
-        return false;
-    }
-}
+/** Mails sendable to system admin */
+const ALLOW_MAIL_SYSTEM_EXCEPTION = ALLOWED_MAILS ? true : false;
+
+/** Mails sendable to site admins */
+const ALLOW_MAIL_SYSTEM_SIGNUP = ALLOWED_MAILS ? true : false;
+
+/** Mails sendable to site users */
+const ALLOW_MAIL_USER_WELCOME = ALLOWED_MAILS ? false : false;
+const ALLOW_MAIL_USER_SIGNIN = ALLOWED_MAILS ? true : false;
+
+/** End Allowed Mail Constants */
--- app/Helpers/system.php
+++ app/Helpers/system.php
@@ -1,35 +1,39 @@
 <?php
 
+/** Site Constants */
+
 const SITE_NAME = "Template";
 const SITE_NAME_SHORT = "Template";
+
+const SITE_LOGO_PRIMARY = "assets/adminLte/dist/img/AdminLTELogo.png";
+const SITE_LOGO_SECONDARY = "assets/adminLte/dist/img/AdminLTELogo.png";
+
+/** End Site Constants */
+
+/** Query Constants */
 
 const DEFAULT_PAGINATE = 10;
 const DEFAULT_ORDER_BY = 'created_at';
 const DEFAULT_ORDER = 'desc';
 
+/** End Query Constants */
+
+/** Fillable Input Constants */
+
 const PASSWORD_MIN_LENGTH = 6;
 const PASSWORD_MAX_LENGTH = 15;
 
-const SITE_LOGO_PRIMARY = "assets/adminLte/dist/img/AdminLTELogo.png";
-const SITE_LOGO_SECONDARY = "assets/adminLte/dist/img/AdminLTELogo.png";
-const SITE_LOGO_MAIL = "assets/adminLte/dist/img/AdminLTELogo.png";
+/** End Fillable Input Constants */
+
+/** Dashboard View Constants */
 
 const DARK_MODE = false;
 
-/** Mail Defaults */
-
-const ALERT_SIGNIN_MAIL = true;
+/** End Dashboard View Constants */
 
 /** Queues Constants */
 
 const QUEUE_EXCEPTION = "queueException";
 const QUEUE_SYSTEM = "queueSystem";
+const QUEUE_ADMIN = "queueAdmin";
 const QUEUE_USER = "queueUser";
-
-/** Mails that are Allowed */
-
-const ALLOW_MAIL_SYSTEM_EXCEPTION = true;
-const ALLOW_MAIL_SYSTEM_SIGNUP = true;
-
-const ALLOW_MAIL_USER_WELCOME = true;
-const ALLOW_MAIL_USER_SIGNIN = true;
